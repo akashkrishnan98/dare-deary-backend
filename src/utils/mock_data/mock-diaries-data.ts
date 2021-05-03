1d8654fa54919f586c8ef32256e7f89930701410
Akash Krishnan <akashkrishnan98@gmail.com>
2021-05-03 08:51:30 +0530

    DEV: Tests CI/CD

--- src/utils/mock_data/mock-diaries-data.ts
+++ src/utils/mock_data/mock-diaries-data.ts
@@ -10,6 +10,15 @@ const MockDiariesData: IDiary[] = [
     createdAt: new Date(),
     lastModifiedAt: new Date(),
   },
+  {
+    id: "2",
+    userEmail: "akashkrishnan98@gmail.com",
+    diaryDate: new Date(),
+    title: "My first Diary",
+    content: "Dear Diary..., Today, I setup the git CI/CD for myself",
+    createdAt: new Date(),
+    lastModifiedAt: new Date(),
+  },
 ];
 
 export default MockDiariesData;
