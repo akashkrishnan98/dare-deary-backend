5254b9c35f51d2d7eb5a343aeb50b558de536d96
Akash Krishnan <akashkrishnan98@gmail.com>
2021-05-03 08:47:59 +0530

    FIX: dist removed

--- dist/utils/mock_data/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MockDiariesData = void 0;
-var mock_diaries_data_1 = __importDefault(require("./mock-diaries-data"));
-exports.MockDiariesData = mock_diaries_data_1.default;
-//# sourceMappingURL=index.js.map
